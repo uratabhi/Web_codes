8322c52e841dcf94e4692526dff2587e14047884
Abhishek Jaiswal <waitabhi2015@gmail.com>
2023-07-10 20:54:57 +0530

    more is added

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
 myForm.addEventListener('submit', onsubmit);
 function onsubmit(e){
     e.preventDefault();
-    if(nameInput.value === '' || emailInput.value === '') {
+    if(nameInput.value === '' || emailInput.value === '' || phoneInput.value==='') {
         // alert('Please enter all fields');
         msg.classList.add('error');
         msg.innerHTML = 'Please enter all fields';
@@ -16,13 +17,16 @@ function onsubmit(e){
         setTimeout(() => msg.remove(), 3000);
       } 
       else{
-         var ptr = localStorage.length;
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`Username=${nameInput.value} Email= ${emailInput.value} Phone= ${phoneInput.value}`));
+        userList.appendChild(li);
          let myobj = {
             username :  nameInput.value,
             useremail : emailInput.value,
+            userphone : phoneInput.value,
          };
          let myobj_serilized = JSON.stringify(myobj);
-         localStorage.setItem(`myobj${ptr}`, myobj_serilized);
+         localStorage.setItem(`${nameInput.value}`, myobj_serilized);
       }
       
 }
