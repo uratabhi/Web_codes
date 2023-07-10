db8b3bdae14a90f9010097872d162e5ca3769526
Abhishek Jaiswal <waitabhi2015@gmail.com>
2023-07-10 15:15:28 +0530

    new feature has been added

--- dom.js
+++ dom.js
@@ -11,8 +11,16 @@
 // headertitle.innerHTML = '<h3>hello world</h3>'
 //  var header = document.getElementById('main-header');
 //  header.style.borderBottom = 'solid 3px black';
-headertitle.style.border = 'solid 3px black';
-var title = document.getElementsByClassName('title');
-console.log(title);
-title[0].style.fontWeight = 'bold';
-title[0].style.color = 'green';
+// headertitle.style.border = 'solid 3px black';
+// var title = document.getElementsByClassName('title');
+// console.log(title);
+// title[0].style.fontWeight = 'bold';
+// title[0].style.color = 'green';
+
+
+ var items = document.getElementsByClassName('list-group-item');
+ items[2].style.backgroundColor = 'green';
+
+ for(var i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+ }
