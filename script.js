5bfadf390b9674ad73b4b7ee7dcddc0de79e47f7
Abhishek Jaiswal <waitabhi2015@gmail.com>
2023-07-11 18:55:36 +0530

    edit functionality added

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function saveToLocalStorage(e){
    const obj = {
      name,
      email,
-     phone,
+     phone
    };
    localStorage.setItem(obj.email, JSON.stringify(obj));
    showNewUserOnScreen(obj);
@@ -18,13 +18,22 @@ function saveToLocalStorage(e){
 function showNewUserOnScreen(user){
       const parentNode = document.getElementById('users');
       const childNode = `<li id=${user.email}>userName=${user.name} <br> userEmail=${user.email} <br> userPhone=${user.phone} 
-      <button class = "delete" onclick=deleteUser('${user.email}')>Delete </button> </li>`
+      <button class = "delete" onclick=deleteUser('${user.email}')>Delete </button> 
+      <button class = "edit"  onclick=editUser('${user.name}','${user.email}','${user.phone}')>Edit </button>
+      </li>`
       parentNode.innerHTML = parentNode.innerHTML+childNode;
     }
 
 function deleteUser(emailId){
      localStorage.removeItem(emailId);
      removeUserFromScreen(emailId);
+}
+
+function editUser(userName, emailId, phoneNumber){
+        document.getElementById('name').value= userName;
+        document.getElementById('email').value= emailId;
+        document.getElementById('phone').value = phoneNumber;
+        deleteUser(emailId);
 }
 
 function removeUserFromScreen(emailId){
